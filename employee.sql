aa8c6308d602ae74793f4e610a48b578ba42a676
kbrantner <brantner.kelsey@gmail.com>
2023-03-26 21:22:50 -0700

    Final query code

--- employee.sql
+++ employee.sql
@@ -25,3 +25,44 @@ SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
   ON e.emp_no = dm.emp_no
  inner JOIN departments d
   ON dm.dept_no = d.dept_no;
+
+  SELECT de.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+ FROM employees e
+ inner JOIN dept_emp de
+  ON   e.emp_no =de.emp_no
+ inner JOIN departments d
+  ON de.dept_no = d.dept_no;
+  
+
+ SELECT e.first_name, e.last_name, e.sex
+ FROM employees e
+ where e.last_name Like 'B%' and e.first_name='Hercules';
+ 
+
+ SELECT e.emp_no, e.last_name, e.first_name
+ FROM employees e
+ inner JOIN dept_emp de
+  ON   e.emp_no =de.emp_no
+ inner JOIN departments d
+  ON de.dept_no = d.dept_no
+  where d.dept_name='Sales';
+ 
+
+ SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+ FROM employees e
+ inner JOIN dept_emp de
+  ON   e.emp_no =de.emp_no
+ inner JOIN departments d
+  ON de.dept_no = d.dept_no
+  where d.dept_name='Sales' or d.dept_name='Development';
+  
+
+ SELECT count(last_name), last_name
+FROM employees e
+where last_name=last_name
+group by last_name
+ORDER BY count(last_name) DESC;
+
+
+  
+  
